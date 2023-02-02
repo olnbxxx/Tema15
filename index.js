2142054c290a5f40bd9ea9d7c297202192527abd
olnbxxx <117682412+olnbxxx@users.noreply.github.com>
2023-02-02 21:17:05 +0100

    terminado

--- index.js
+++ index.js
@@ -12,9 +12,8 @@ console.log(sessionStorage.getItem("nombre"));
 localStorage.setItem("apellido", apellido);
 console.log(localStorage.getItem("apellido"));
 
-localStorage.setItem("person", JSON.stringify { nome: "luis", anni: 46 })
+localStorage.setItem("person", JSON.stringify(persona))
 console.log(JSON.parse(localStorage.getItem("person")))
 
-document.cookie = "personcookie;expires="
-expires="  + new Date
-console.log(document.cookie)
+const now = new Date();
+document.cookie = `oscar=${JSON.stringify(persona)};expires=${new Date(now.getTime() + 2 * 60000)}`
